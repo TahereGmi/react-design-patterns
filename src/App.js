67f0b99a4d4054479c9603edb559a0972b509889
TahereGmi <mstgholami@gmail.com>
2023-01-27 20:07:46 +0330

    Create Forms with HOCs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import { printProps } from "./printProps"
-import { UserInfo } from './UserInfo'
-import { withUser } from "./withUser";
+import { UserInfoForm } from './UserInfoForm'
 
-const UserInfoWithLoader = withUser(UserInfo, '123')
 
 function App() {
   return (
-    // Loading data in withUser (Higher Order Component)
-    <UserInfoWithLoader />
+    <UserInfoForm />
   );
 }
 
--- src/withEditableUser.js
+++ src/withEditableUser.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import axios from 'axios'
 
-export const withEditableUser = ({ Component, userId }) => {
+export const withEditableUser = (Component, userId) => {
     return props => {
         const [originalUser, setOriginalUser] = useState(null)
         const [user, setUser] = useState(null)
